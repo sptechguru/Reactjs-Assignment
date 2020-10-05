ca11d3594b8f2e242f437a58d8443e93302d1a5d
santosh pal <58684635+sptechguru@users.noreply.github.com>
2020-10-05 11:45:32 +0530

    Update SidebarData.js

--- Frontend/Boxito Assignment Frontend/src/components/SidebarData.js
+++ Frontend/Boxito Assignment Frontend/src/components/SidebarData.js
@@ -1,9 +1,5 @@
 import React from 'react';
-import * as FaIcons from 'react-icons/fa';
-import * as AiIcons from 'react-icons/fa';
-import * as IoIcons from 'react-icons/io';
-import * as FaAngellist from "react-icons/fa";
-import { AiOutlineBarcode } from "react-icons/fa";
+
 
 export const SidebarData = [
   {
